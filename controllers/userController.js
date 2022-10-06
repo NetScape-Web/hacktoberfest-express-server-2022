b339c77b94c46f6c406b1191d79348a8d02a435b
Sonu Kumar <sonu.patna0808@gmail.com>
2022-10-06 13:59:01 +0530

    Adding User login route to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,4 +49,44 @@ const userRegistration = async (req, res) => {
   }
 };
 
-export { userRegistration };
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email && password) {
+      const user = await UserModel.findOne({ email: email });
+      if (user != null) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (user.email === email && isMatch) {
+          // Generate JWT Token
+          const token = jwt.sign(
+            { userID: user._id },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "5d" }
+          );
+          res.send({
+            status: "success",
+            message: "Login Success",
+            token: token,
+          });
+        } else {
+          res.send({
+            status: "failed",
+            message: "Email or Password is not Valid",
+          });
+        }
+      } else {
+        res.send({
+          status: "failed",
+          message: "You are not a Registered User",
+        });
+      }
+    } else {
+      res.send({ status: "failed", message: "All Fields are Required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: "failed", message: "Unable to Login" });
+  }
+};
+
+export { userRegistration, userLogin };
